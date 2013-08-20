4b29444ee1c005a6980744a24deb253c4f62c3e3
nadams <nadams@scentsy.com>
2013-08-20 15:34:54 -0600

    Setting theme to Holo

--- src/net/node3/freelancerdatabase/MainActivity.scala
+++ src/net/node3/freelancerdatabase/MainActivity.scala
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import android.view.Menu
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.ListView
 import android.widget.Toast
 import net.node3.freelancerdatabase.db.StarSystemRegistry
@@ -11,14 +12,14 @@ import net.node3.freelancerdatabase.db.StarSystemRegistry
 class MainActivity extends Activity with StarSystemRegistry {
   lazy val navLayout = findViewById(R.id.main_drawer_layout)
   lazy val navList = findViewById(R.id.main_nav_list).asInstanceOf[ListView]
+  lazy val searchBox = findViewById(R.id.main_system_filter).asInstanceOf[EditText]
   
   override def onCreate(savedInstanceState: Bundle) = {
     super.onCreate(savedInstanceState)
     
     setContentView(R.layout.activity_main)
     
-    val systems = systemRepository.getAll.toArray
-    navList.setAdapter(new ArrayAdapter(this, R.layout.drawer_list_item, systems))
+    navList.setAdapter(new ArrayAdapter(this, R.layout.drawer_list_item, systemRepository.getAll.toArray))
     navList.onItemClick { (parent, view, position, id) => Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show }
   }
 
