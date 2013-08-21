2dc73ac83f45e2ca097dfda4acc854bdd0ea6e68
nadams <nadams@scentsy.com>
2013-08-21 07:32:42 -0600

    Adding drawer icon and drawer toggle

--- src/net/node3/freelancerdatabase/MainActivity.scala
+++ src/net/node3/freelancerdatabase/MainActivity.scala
@@ -8,12 +8,18 @@ import android.widget.EditText
 import android.widget.ListView
 import android.widget.Toast
 import net.node3.freelancerdatabase.db.StarSystemRegistry
+import android.support.v4.app.ActionBarDrawerToggle
+import android.support.v4.widget.DrawerLayout
+import android.content.res.Configuration
+import android.view.MenuItem
 
 class MainActivity extends Activity {
-  lazy val navLayout = findViewById(R.id.main_drawer_layout)
+  lazy val systemRepository = StarSystemRegistry.systemRepository(this)
+  
+  lazy val navLayout = findViewById(R.id.main_drawer_layout).asInstanceOf[DrawerLayout]
   lazy val navList = findViewById(R.id.main_nav_list).asInstanceOf[ListView]
   lazy val searchBox = findViewById(R.id.main_system_filter).asInstanceOf[EditText]
-  lazy val systemRepository = StarSystemRegistry.systemRepository(this)
+  lazy val drawerToggle = new ActionBarDrawerToggle(this, navLayout, R.drawable.ic_drawer, R.string.main_drawer_open, R.string.main_drawer_close)
   
   override def onCreate(savedInstanceState: Bundle) = {
     super.onCreate(savedInstanceState)
@@ -22,7 +28,27 @@ class MainActivity extends Activity {
     
     navList.setAdapter(new ArrayAdapter(this, R.layout.drawer_list_item, systemRepository.getAll.toArray))
     navList.onItemClick { (parent, view, position, id) => Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show }
+    navLayout.setDrawerListener(drawerToggle)
+    
+    val actionBar = getActionBar
+    actionBar.setDisplayHomeAsUpEnabled(true)
+    actionBar.setHomeButtonEnabled(true)
   }
+  
+  override def onPostCreate(savedInstanceState: Bundle) = {
+    super.onPostCreate(savedInstanceState)
+    
+    drawerToggle.syncState
+  }
+  
+  override def onConfigurationChanged(newConfig: Configuration) = {
+    super.onConfigurationChanged(newConfig)
+    drawerToggle.onConfigurationChanged(newConfig)
+  }
+  
+  override def onOptionsItemSelected(item: MenuItem) = 
+    if(drawerToggle.onOptionsItemSelected(item)) true 
+    else super.onOptionsItemSelected(item)
 
   override def onCreateOptionsMenu(menu: Menu) = {
     getMenuInflater.inflate(R.menu.main, menu)
