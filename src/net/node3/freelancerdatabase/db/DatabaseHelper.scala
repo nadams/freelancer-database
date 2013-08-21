409ca8abb3296f2bf98dcd2d67e78f31a9d7c652
nadams <nadams@scentsy.com>
2013-08-21 09:25:32 -0600

    Creating a list of the tables needed in the DatabaseHelper class

--- src/net/node3/freelancerdatabase/db/DatabaseHelper.scala
+++ src/net/node3/freelancerdatabase/db/DatabaseHelper.scala
@@ -3,9 +3,22 @@ package net.node3.freelancerdatabase.db
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import net.node3.freelancerdatabase.db.tables.SectorTable
+import net.node3.freelancerdatabase.db.tables.StarSystemTable
+import net.node3.freelancerdatabase.db.tables.SolarObjectTypeTable
+import net.node3.freelancerdatabase.db.tables.SolarObjectTable
+import net.node3.freelancerdatabase.db.tables.SystemConnectionTable
 
-class DatabaseHelper(val context: Context, val tables: List[TableHelper])
+class DatabaseHelper(val context: Context)
   extends SQLiteOpenHelper(context, DatabaseHelper.databaseName, null, DatabaseHelper.databaseVersion) {
+  
+  private val tables = List[TableHelper](
+    SectorTable,
+    SolarObjectTypeTable,
+    StarSystemTable,
+    SolarObjectTable,
+    SystemConnectionTable
+  )
   
   override def onCreate(db: SQLiteDatabase) = {
     if (!db.isReadOnly()) {
