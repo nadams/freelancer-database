e4efb812dd71473d1450b2d7a2d0f212941810b8
nadams <nadams@scentsy.com>
2013-08-21 08:17:56 -0600

    Implementing the onUpgrade method

--- src/net/node3/freelancerdatabase/db/TableHelper.scala
+++ src/net/node3/freelancerdatabase/db/TableHelper.scala
@@ -1,10 +1,12 @@
 package net.node3.freelancerdatabase.db
 
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
 
 abstract class TableHelper {
-  val logTag = "DBHelper"
-  val revisions = Map[Int, TableRevision]()
+  private val logTag = "DBHelper"
+  private val revisions = scala.collection.mutable.Map[Int, TableRevision]()
+  private var newestRevision = 0
   
   def onCreate(database: SQLiteDatabase) = {
     revisions.headOption match {
@@ -17,6 +19,29 @@ abstract class TableHelper {
   }
   
   def onUpgrade(database: SQLiteDatabase, oldVersion: Int, newVersion: Int) = {
+    for(version <- Array.range(oldVersion + 1, newVersion)) {
+      if(revisions.contains(version) && revisions.head._1 != version) {
+        database.beginTransaction
+        
+        try {
+          revisions(version).applyRevision(database)
+          database.setTransactionSuccessful
+        } catch {
+          case e: SQLiteException => Unit
+        } finally {
+          database.endTransaction
+        }
+      }
+    }
+  }
+  
+  def registerRevision(revision: TableRevision) = {
+    val revisionNumber = revision.getRevisionNumber
     
+    if(revisionNumber > newestRevision) {
+      newestRevision = revisionNumber
+    }
+    
+    revisions.put(revisionNumber, revision)
   }
 }
