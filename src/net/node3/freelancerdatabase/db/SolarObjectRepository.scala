1e3b00915342a0b7c0766c564ebe76bc628c827d
nadams <nadams@scentsy.com>
2013-08-26 09:17:19 -0600

    Implementing some of the repository methods

--- src/net/node3/freelancerdatabase/db/SolarObjectRepository.scala
+++ src/net/node3/freelancerdatabase/db/SolarObjectRepository.scala
@@ -2,6 +2,7 @@ package net.node3.freelancerdatabase.db
 
 import android.content.Context
 import net.node3.freelancerdatabase.entities.SolarObject
+import net.node3.freelancerdatabase.db.tables.SolarObjectTable
 
 trait SolarObjectRepositoryComponent {
   def solarObjectRepository(context: Context) : SolarObjectRepository
@@ -11,19 +12,34 @@ trait SolarObjectRepository {
   def getById(id: Int): Option[SolarObject]
   def getAllBySystemId(id: Int): Seq[SolarObject]
   def add(solarObject: SolarObject): Option[Long]
-  def update(id: Int, newSolarObject: SolarObject): Boolean
-  def delete(id: Int): Boolean
+  def update(id: Int, newSolarObject: SolarObject): Int
+  def delete(id: Int): Int
 }
 
 trait SolarObjectRepositoryComponentImpl extends SolarObjectRepositoryComponent {
   def solarObjectRepository(context: Context) = new SolarObjectRepositoryImpl(context)
   
   class SolarObjectRepositoryImpl(val context: Context) extends SolarObjectRepository {
+    val database = new DatabaseHelper(context)
+    
     def getById(id: Int) = ???
     def getAllBySystemId(id: Int) = ???
     def add(solarObject: SolarObject) = ???
-    def update(id: Int, newSolarObject: SolarObject) = ???
-    def delete(id: Int) = ???
+    
+    def update(id: Int, newSolarObject: SolarObject) = 
+      database.getWritableDatabase.update(
+        SolarObjectTable.tableName, 
+        newSolarObject.toContentValues, 
+        f"${SolarObjectTable.id} = ?", 
+        Array(id.toString)
+      )
+    
+    def delete(id: Int) = 
+      database.getWritableDatabase.delete(
+        SolarObjectTable.tableName, 
+        f"${SolarObjectTable.id} = ?", 
+        Array(id.toString)
+      )
   }
 }
 
--- src/net/node3/freelancerdatabase/entities/SolarObject.scala
+++ src/net/node3/freelancerdatabase/entities/SolarObject.scala
@@ -1,3 +1,25 @@
 package net.node3.freelancerdatabase.entities
 
-case class SolarObject(id: Int, name: String, x: Double, y: Double, solarObjectType: SolarObjectType)
+import android.content.ContentValues
+import net.node3.freelancerdatabase.db.tables.SolarObjectTable
+import android.database.Cursor
+
+case class SolarObject(id: Int, name: String, x: Double, y: Double, solarObjectType: SolarObjectType) {
+  def toContentValues() : ContentValues = {
+    val values = new ContentValues
+    
+    values.put(SolarObjectTable.id, id.toString)
+    values.put(SolarObjectTable.name, name)
+    values.put(SolarObjectTable.x, x)
+    values.put(SolarObjectTable.y, y)
+    values.put(SolarObjectTable.solar_object_type_id, solarObjectType.id.toString)
+    
+    // TODO: Take care of star system
+    
+    values
+  }
+}
+
+object SolarObject {
+  def apply(cursor: Cursor) = ???
+}
