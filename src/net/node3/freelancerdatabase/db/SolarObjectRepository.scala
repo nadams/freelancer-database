81d338c4ad1e4e64273646ff91197f4f6b36b128
nadams <nadams@scentsy.com>
2013-08-26 11:28:47 -0600

    Implementing getById for SolarObjectRepository

--- src/net/node3/freelancerdatabase/db/SolarObjectRepository.scala
+++ src/net/node3/freelancerdatabase/db/SolarObjectRepository.scala
@@ -3,9 +3,12 @@ package net.node3.freelancerdatabase.db
 import android.content.Context
 import net.node3.freelancerdatabase.entities.SolarObject
 import net.node3.freelancerdatabase.db.tables.SolarObjectTable
+import net.node3.freelancerdatabase.db.tables.SolarObjectTypeTable
+import android.database.Cursor
+import net.node3.freelancerdatabase.entities.SolarObjectType
 
 trait SolarObjectRepositoryComponent {
-  def solarObjectRepository(context: Context) : SolarObjectRepository
+  def solarObjectRepository(context: Context): SolarObjectRepository
 }
 
 trait SolarObjectRepository {
@@ -18,28 +21,43 @@ trait SolarObjectRepository {
 
 trait SolarObjectRepositoryComponentImpl extends SolarObjectRepositoryComponent {
   def solarObjectRepository(context: Context) = new SolarObjectRepositoryImpl(context)
-  
+
   class SolarObjectRepositoryImpl(val context: Context) extends SolarObjectRepository {
     val database = new DatabaseHelper(context)
-    
-    def getById(id: Int) = ???
+
+    def getById(id: Int): Option[SolarObject] = {
+      val sql =
+        f"""
+        	SELECT 
+        		so.${SolarObjectTable.id},
+        		so.${SolarObjectTable.name},
+        		so.${SolarObjectTable.x},
+        		so.${SolarObjectTable.y},
+        		so.${SolarObjectTable.solar_object_type_id},
+        		sat.${SolarObjectTypeTable.name}
+        	FROM ${SolarObjectTable.tableName} AS so
+      			INNER JOIN ${SolarObjectTypeTable.tableName} AS sat ON so.${SolarObjectTable.solar_object_type_id} = sat.${SolarObjectTypeTable.id} 
+      		WHERE ${SolarObjectTable.id} = ?
+        """
+
+      SolarObject(database.getReadableDatabase.rawQuery(sql, Array(id.toString)))
+    }
+
     def getAllBySystemId(id: Int) = ???
     def add(solarObject: SolarObject) = ???
-    
-    def update(id: Int, newSolarObject: SolarObject) = 
+
+    def update(id: Int, newSolarObject: SolarObject) =
       database.getWritableDatabase.update(
-        SolarObjectTable.tableName, 
-        newSolarObject.toContentValues, 
-        f"${SolarObjectTable.id} = ?", 
-        Array(id.toString)
-      )
-    
-    def delete(id: Int) = 
+        SolarObjectTable.tableName,
+        newSolarObject.toContentValues,
+        f"${SolarObjectTable.id} = ?",
+        Array(id.toString))
+
+    def delete(id: Int) =
       database.getWritableDatabase.delete(
-        SolarObjectTable.tableName, 
-        f"${SolarObjectTable.id} = ?", 
-        Array(id.toString)
-      )
+        SolarObjectTable.tableName,
+        f"${SolarObjectTable.id} = ?",
+        Array(id.toString))
   }
 }
 
--- src/net/node3/freelancerdatabase/entities/SolarObject.scala
+++ src/net/node3/freelancerdatabase/entities/SolarObject.scala
@@ -5,21 +5,33 @@ import net.node3.freelancerdatabase.db.tables.SolarObjectTable
 import android.database.Cursor
 
 case class SolarObject(id: Int, name: String, x: Double, y: Double, solarObjectType: SolarObjectType) {
-  def toContentValues() : ContentValues = {
+  def toContentValues(): ContentValues = {
     val values = new ContentValues
-    
+
     values.put(SolarObjectTable.id, id.toString)
     values.put(SolarObjectTable.name, name)
     values.put(SolarObjectTable.x, x)
     values.put(SolarObjectTable.y, y)
     values.put(SolarObjectTable.solar_object_type_id, solarObjectType.id.toString)
-    
+
     // TODO: Take care of star system
-    
+
     values
   }
 }
 
 object SolarObject {
-  def apply(cursor: Cursor) = ???
+  def apply(cursor: Cursor) =
+    if (cursor.moveToFirst()) {
+      Some(new SolarObject(
+        cursor.getInt(0),
+        cursor.getString(1),
+        cursor.getDouble(2),
+        cursor.getDouble(3),
+        SolarObjectType(
+          cursor.getInt(4),
+          cursor.getString(5))))
+    } else {
+      None
+    }
 }
